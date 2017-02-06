7aa992b160411c649a7f7155046568c81acbd98a
Cosimo89 <cosimo.livi@gmail.com>
2017-02-06 17:17:05 +0100

    added trig functions

--- assignment 1/assignment1.py
+++ assignment 1/assignment1.py
@@ -1,13 +1,19 @@
 from numpy import *
 from pylab import *
 
-Input=input("Please select a function: \n 1: x ")
+Input=input("Please select a function: \n 1: x \n 2: sin(x) \n 3: cos(x) \n 4: tan(x) ")
 
 xval=arange(-5.0,5.0,0.1)
 
 
 if Input == 1:
     yval=xval 
+if Input == 2:
+    yval=sin(xval)
+if Input == 3:
+    yval=cos(xval)
+if Input == 4:
+    yval=tan(xval)
 
 plot (xval,yval, 'b')
 show()
