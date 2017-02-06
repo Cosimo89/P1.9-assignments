a99f8bb48c009ee36365f39799d9a97c8522714d
Cosimo89 <cosimo.livi@gmail.com>
2017-02-06 17:48:12 +0100

    reintroduced polynomial, they had been deleted by en error in fixing conflicts

--- assignment 1/assignment1.py
+++ assignment 1/assignment1.py
@@ -4,7 +4,7 @@ import sys
 
 if len(sys.argv)==1:
 
-    Input=input("Please select a function: \n 1: x \n 2: sin(x) \n 3: cos(x) \n 4: tan(x) ")
+    Input=input("Please select a function: \n 1: x \n 2: x**2\n 3: x**3\n 4: sin(x) \n 5: cos(x) \n 6: tan(x) ")
 
 else:
     Input=(int)(sys.argv[1])
@@ -19,10 +19,14 @@ xval=arange(-3.0,3.0,0.1)
 if Input == 1:
     yval=xval 
 if Input == 2:
-    yval=sin(xval)
+    yval=xval**2 
 if Input == 3:
-    yval=cos(xval)
+    yval=xval**3 
 if Input == 4:
+    yval=sin(xval)
+if Input == 5:
+    yval=cos(xval)
+if Input == 6:
     yval=tan(xval)
 
 
