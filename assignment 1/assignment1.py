3f1598f55d3c8684b9868b01f1b6263859d7da72
Cosimo89 <cosimo.livi@gmail.com>
2017-02-06 17:33:00 +0100

    introduced irrational functions

--- assignment 1/assignment1.py
+++ assignment 1/assignment1.py
@@ -5,7 +5,7 @@ import sys
 
 if len(sys.argv)==1:
 
-    Input=input("Please select a function: \n 1: x ")
+    Input=input("Please select a function: \n 1: x \n 2: exp(x) \n 3: sqrt(x) \n")
 else:
     Input=(int)(sys.argv[1])
 
@@ -15,6 +15,10 @@ xval=arange(-5.0,5.0,0.1)
 
 if Input == 1:
     yval=xval 
+if Input == 2:
+    yval=exp(xval)
+if Input == 3:
+    yval=sqrt(xval)
 
 plot (xval,yval, 'b')
 show()
