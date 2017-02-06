dc4a217d8c5b7a60f4d4503a8101f9c132df518d
Cosimo89 <cosimo.livi@gmail.com>
2017-02-06 17:11:14 +0100

    added polynomial functions

--- assignment 1/assignment1.py
+++ assignment 1/assignment1.py
@@ -1,13 +1,17 @@
 from numpy import *
 from pylab import *
 
-Input=input("Please select a function: \n 1: x ")
+Input=input("Please select a function: \n 1: x \n 2: x**2 \n 3: x**3 \n")
 
 xval=arange(-5.0,5.0,0.1)
 
 
 if Input == 1:
     yval=xval 
+if Input == 2:
+    yval=xval**2
+if Input == 3:
+    yval=xval**3
 
 plot (xval,yval, 'b')
 show()
