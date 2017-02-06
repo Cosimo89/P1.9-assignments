df31d077ce2686d817865d568382cccff44f5166
Cosimo89 <cosimo.livi@gmail.com>
2017-02-06 17:29:56 +0100

    insercted argument passing for developers

--- assignment 1/assignment1.py
+++ assignment 1/assignment1.py
@@ -1,7 +1,14 @@
 from numpy import *
 from pylab import *
 
-Input=input("Please select a function: \n 1: x ")
+import sys
+
+if len(sys.argv)==1:
+
+    Input=input("Please select a function: \n 1: x ")
+else:
+    Input=(int)(sys.argv[1])
+
 
 xval=arange(-5.0,5.0,0.1)
 
